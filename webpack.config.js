10ce9bdd4afa36e3af2d34c6b97b5c4b50045d58
mish-hash <poluektov23@gmail.com>
2022-08-15 18:22:55 +0300

    add webpack clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         main: path.resolve(__dirname, './js/script.js')
     },
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './dist/js'),
         filename: '[name].bundle.js',
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
 };
